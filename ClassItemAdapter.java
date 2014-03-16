3f529cf50f4715ef7caac85e3222153cb116d1c1
resgestrae <resgestrae@users.noreply.github.com>
2014-03-16 19:48:44 -0400

    Update ClassItemAdapter.java

--- ClassItemAdapter.java
+++ ClassItemAdapter.java
@@ -1,4 +1,4 @@
-package com.example.toastlistviewexample4;
+package com.android.nest;
 
 import java.util.ArrayList;
 import android.content.Context;
@@ -13,8 +13,8 @@ public class ClassItemAdapter extends ArrayAdapter<ClassItem> {
 	private ArrayList<ClassItem> classItem;
 
 	// Override ArrayAdapter Constructor
-	public ClassItemAdapter(Context context, int textViewResourceId,
-			ArrayList<ClassItem> classItem) {
+	public ClassItemAdapter(Context context, int textViewResourceId,ArrayList<ClassItem> classItem)
+	{
 		super(context, textViewResourceId, classItem);
 		this.classItem = classItem;
 	}
@@ -35,10 +35,10 @@ public class ClassItemAdapter extends ArrayAdapter<ClassItem> {
 			// Obtain a reference to the TextViews
 			// TextViews are created in XML files we define
 			TextView cn = (TextView) v.findViewById(R.id.classNameTV);
-			TextView ct = (TextView) v.findViewById(R.id.classNameTV);
+			TextView ct = (TextView) v.findViewById(R.id.classTimeTV);
 			TextView bn = (TextView) v.findViewById(R.id.buildingNameTV);
 			TextView rn = (TextView) v.findViewById(R.id.roomNumTV);
-
+			TextView dayTv = (TextView) v.findViewById(R.id.dayTV);
 			// Check if each text view is null, if not assign text
 			if (cn != null) {
 				cn.setText(i.getClassName());
@@ -51,6 +51,10 @@ public class ClassItemAdapter extends ArrayAdapter<ClassItem> {
 			}
 			if (rn != null) {
 				rn.setText(i.getRoomNum());
+			}
+			if(dayTv != null)
+			{
+				dayTv.setText(i.getDay());
 			}
 		}
 		// Return the view to activity
